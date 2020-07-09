cac1893b1d9dc84e520a11a9978088fdf7314e1c
vagrant <vagrant@localhost.localdomain>
2020-07-09 05:22:18 +0000

    add model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise  :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable
   has_many :boards, dependent: :destroy
   has_many :bookmraks, dependent: :destroy
   has_many :board_comments, dependent: :destroy
-  has_many :relationships
+  has_many :favorites, dependent: :destroy
+
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy 
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy 
+  has_many :following_user, through: :follower, source: :followed
+  has_many :follower_user, through: :followed, source: :follower
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_07_121015) do
+ActiveRecord::Schema.define(version: 2020_07_09_042146) do
 
   create_table "board_comments", force: :cascade do |t|
     t.integer "user_id"
@@ -39,6 +39,21 @@ ActiveRecord::Schema.define(version: 2020_07_07_121015) do
   create_table "bookmraks", force: :cascade do |t|
     t.integer "user_id"
     t.integer "board_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "inquiries", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
