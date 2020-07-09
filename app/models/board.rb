78115dd6916d4a1bbd7025baaa3a0d7bd6ab0451
vagrant <vagrant@localhost.localdomain>
2020-07-09 07:02:41 +0000

    route add

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,6 @@
 class Board < ApplicationRecord
+    has_many :board_comments, dependent: :destroy
+    has_many :board_tags, dependent: :destroy
+    has_many :bookmraks, despendent: :destroy
     belongs_to :user
 end
