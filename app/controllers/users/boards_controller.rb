9832959e71ed5f2afe25d0984ca66a05b9ef3be7
vagrant <vagrant@localhost.localdomain>
2020-07-11 14:23:16 +0000

    add

--- app/controllers/users/boards_controller.rb
+++ app/controllers/users/boards_controller.rb
@@ -1,2 +1,19 @@
 class Users::BoardsController < ApplicationController
+
+    def create
+        @board = Board.new(board_params)
+        @board.save
+        redirect_to("/")
+        tag_list = params[:tag_name].split(",")
+        if  @post.save
+            @post.save_posts(tag_list)
+        end
+    end
+    
+    
+    private
+    def board_params
+        params.require(:board).permit(:user_id, :introduction, :title, :image)
+    end
+
 end
