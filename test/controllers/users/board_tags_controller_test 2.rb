246fec9429ecac96c5c8a0a1f6623f97fea686f8
vagrant <vagrant@localhost.localdomain>
2020-07-14 11:26:29 +0000

    add

---
+++ test/controllers/users/board_tags_controller_test 2.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Users::BoardTagsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/controllers/users/favorite_controller_test 2.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Users::FavoriteControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
