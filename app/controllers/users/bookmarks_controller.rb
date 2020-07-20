dfbec5ea16421f2727fa0f1668420e8cd08f1997
vagrant <vagrant@localhost.localdomain>
2020-07-20 16:10:24 +0000

    function

--- app/controllers/users/bookmarks_controller.rb
+++ app/controllers/users/bookmarks_controller.rb
@@ -1,14 +1,14 @@
 class Users::BookmarksController < ApplicationController
     def create
-        
+
         bookmark = current_user.bookmarks.build(board_id: params[:board_id])
         bookmark.save!
-        redirect_to users_boards_path, success: t('.flash.bookmark')
+        redirect_to request.referer
     end
 
     def destroy
         current_user.bookmarks.find_by(board_id: params[:board_id]).destroy!
-        redirect_to boards_path, success: t('.flash.not_bookmark')
+        redirect_to request.referer
     end
 
     def index
