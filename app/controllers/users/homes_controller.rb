68c0d66d9065212eec7bcc455903b10bf04da175
vagrant <vagrant@localhost.localdomain>
2020-07-26 06:55:45 +0000

    add c

--- app/controllers/users/homes_controller.rb
+++ app/controllers/users/homes_controller.rb
@@ -1,7 +1,9 @@
 class Users::HomesController < ApplicationController
     def top
         #タグをランダム表示
-        @random = Tag.order("RAND()").limit(5)
+        # @random = Tag.order("RAND()").limit(5)→RANDAM関数はmysqlでは使用できない.RANDはsqliteで使用できない。
+        tag_ids = Tag.pluck(:id).sample(5)
+        @random = Tag.where(id: tag_ids)
         #スレッドをブックマークの多い順で表示
         @all_ranks = Board.find(Bookmark.group(:board_id).order('count(board_id) desc').limit(3).pluck(:board_id))
     end
