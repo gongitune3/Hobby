66a56e6d423c0a754387d9692c6de5a783aaeed6
gongitune3 <rtdmwrtdmw@gmail.com>
2020-08-11 01:52:02 +0000

    bookmark ajax add

--- app/controllers/users/bookmarks_controller.rb
+++ app/controllers/users/bookmarks_controller.rb
@@ -1,16 +1,16 @@
 class Users::BookmarksController < ApplicationController
     def create
+        @board = Board.find(params[:board_id])
         bookmark = current_user.bookmarks.build(board_id: params[:board_id])
         bookmark.save
         # redirect_to request.referer
-        redirect_back(fallback_location: root_path)
     end
 
     def destroy
+        @board = Board.find(params[:board_id])
         bookmark = current_user.bookmarks.find_by(board_id: params[:board_id])
         bookmark.destroy
         # redirect_to request.referer
-        redirect_back(fallback_location: root_path)
     end
 
     def index
