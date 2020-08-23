f6def4ba710ebc83663739b453f281f60a7ab138
gongitune3 <rtdmwrtdmw@gmail.com>
2020-08-23 08:16:41 +0000

    add

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_035703) do
+ActiveRecord::Schema.define(version: 2020_08_20_114137) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email"
@@ -62,8 +62,9 @@ ActiveRecord::Schema.define(version: 2020_08_20_035703) do
     t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "type"
+    t.integer "category"
     t.boolean "reply", default: false, null: false
+    t.integer "status", default: 0, null: false
   end
 
   create_table "favorites", force: :cascade do |t|
