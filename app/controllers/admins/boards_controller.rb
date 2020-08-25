57cd8963a04df483c7624fa41921527331fae4fc
gongitune3 <rtdmwrtdmw@gmail.com>
2020-08-25 09:52:51 +0000

    admin view adjust

--- app/controllers/admins/boards_controller.rb
+++ app/controllers/admins/boards_controller.rb
@@ -1,6 +1,10 @@
 class Admins::BoardsController < ApplicationController
+
       def index
+            @boards = Board.all.order(created_at: :desc)
+      end
+
+      def edit
             
       end
-      
 end
