3eada0d6f31da8a609227d3df2ee6bed0a06b3b9
gongitune3 <rtdmwrtdmw@gmail.com>
2020-08-26 11:28:35 +0000

    test

--- bash.sh
+++ bash.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-DIR="/home/ec2-user/Hobby"
-DIR_CURRENT=${DIR}/current
-if [[ -d "$DIR_CURRENT" ]]; then
-   cd $DIR_CURRENT
-   echo $DIR_CURRENT
-else
-   cd $DIR
-   echo $DIR
-fi
+# DIR="/home/ec2-user/Hobby"
+# DIR_CURRENT=${DIR}/current
+# if [[ -d "$DIR_CURRENT" ]]; then
+#    cd $DIR_CURRENT
+#    echo $DIR_CURRENT
+# else
+#    cd $DIR
+#    echo $DIR
+# fi
 
-T_DAY=`date +%Y%m%d --date '7 day ago'`
-zip -r ./log/production/${T_DAY}.zip ./log/production/${T_DAY}
-rm -r ./log/production/${T_DAY}
+# T_DAY=`date +%Y%m%d --date '7 day ago'`
+# zip -r ./log/production/${T_DAY}.zip ./log/production/${T_DAY}
+# rm -r ./log/production/${T_DAY}
+
+# T2_DAY=`date +%Y%m%d --date '14 day ago'`
+# rm -r ./log/production/${T2_DAY}.zip
+
+# 権限がない為、一旦上の階層にあげて回避。
