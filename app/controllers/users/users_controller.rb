f83d1fe2f9624173ab45247cc1ad8f8b068867b5
gongitune3 <rtdmwrtdmw@gmail.com>
2020-08-30 08:39:15 +0000

    add

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -7,7 +7,7 @@ class Users::UsersController < ApplicationController
         if @user.profile_image_id.present?
             @image_url = "https://hobby3ch-files-resize.s3-ap-northeast-1.amazonaws.com/store/" + @user.profile_image_id + "-thumbnail."
         else
-            @image_url = image-url("no_image.jpg");
+            @image_url = image_path("no_image.jpg");
         end
 
         order_comment = @user.board_comments.order(created_at: :desc).limit(3)
