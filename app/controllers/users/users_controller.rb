126cddc1fa8b80063f0647e55bec46619cd06770
gongitune3 <rtdmwrtdmw@gmail.com>
2020-08-31 11:23:11 +0900

    adjust

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -14,17 +14,13 @@ class Users::UsersController < ApplicationController
         order_comment = @user.board_comments.order(created_at: :desc).limit(3)
         @order_boards = Board.where(id: order_comment.pluck(:board_id)).distinct
 
-        # ユーザーおすすめ機能、
-        # 自身がコメントした板に紐づくタグと同様のタグをもつ板にコメントしているユーザーを取得する
-
+        # 以下・ユーザーおすすめ機能、自身がコメントした板に紐づくタグと同様のタグをもつ板にコメントしているユーザーを取得する
         # 1/自分のコメントを取得
         comments = @user.board_comments
         # 2/自分がコメントした板の情報をコメントのidを元に取得
         boards = Board.where(id: comments.pluck(:board_id)).distinct
-        
         # 3/自分がコメントしたボードのidから中間テーブルを参照しtag_idsを定義し配列に代入？紐づくタグidを取得。
-        # board_ids = BoardTag.where(tag_id: boards.board_tags.pluck(:tag_id).pluck(:board_id))
-
+        # board_ids = BoardTag.where(tag_id: boards.board_tags.pluck(:tag_id).pluck(:board_id))←boardsがレコードで来ている為使えない。
         tag_ids = []
         boards.each do |board|
             board.board_tags.each do |board_tag|
@@ -33,18 +29,14 @@ class Users::UsersController < ApplicationController
             end
         end
         board_tags = BoardTag.where(tag_id: tag_ids).distinct
-        # board_tags = BoardTag.where(tag_id: boards.board_tags.pluck(:tag_id))        
-
         # 5/ 4を行った上で関連している板のidを検索。
         board_ids = board_tags.pluck(:board_id).uniq
-        
         # //自分を例外にする処理
         user_comments_ids = []
         current_user.board_comments.each do |user|
             user_comments_ids << user.id
         end
-        
-        # 6/ idを持っているコメントを抽出
+        # 6/ 上記で取得してきたidを持っているコメントを抽出
         board_comments = BoardComment.where.not(id: user_comments_ids).where(board_id: board_ids)
         # 7/ コメントからユーザーの情報を取得
         users = []
@@ -52,17 +44,6 @@ class Users::UsersController < ApplicationController
             users << board_comment.user
         end
         @recommended = users.uniq.shuffle.take(3)
-        
-        # a = []
-        # recommende.each do |r|
-        #     if current_user.id =! r.id
-        #         a << r
-        #     end
-        # end
-
-        # @recommended = a
-
-        
     end
     
     def index
@@ -75,6 +56,7 @@ class Users::UsersController < ApplicationController
 
     def update
         @user = User.find(params[:id])
+        # 本番環境と開発環境でアップデートを切り分ける方法
         if @user.update(user_params)
             sleep(3)
             flash.now[:notice] = '更新完了'
