93287e77650d9fbc690dacc4881e3b9d62959432
gongitune3 <rtdmwrtdmw@gmail.com>
2020-10-16 17:16:12 +0900

    add

--- app/controllers/users/homes_controller.rb
+++ app/controllers/users/homes_controller.rb
@@ -8,10 +8,10 @@ class Users::HomesController < ApplicationController
         # @random = Tag.order("RAND()").limit(5)→RANDOM関数はmysqlでは使用できない.RANDはsqlite3で使用できない。
         tag_ids = Tag.joins(:board_tags).distinct.pluck(:id).sample(5)
         @random = Tag.where(id: tag_ids)
-        
+
         #スレッドをブックマークの多い順で表示
         @all_ranks = Board.find(Bookmark.group(:board_id).order('count(board_id) desc').limit(3).pluck(:board_id))
-        
+
         # @lines = BoardComment.order("RANDOM()").limit(3)
         # ホビ子のセリフをランダムで
         line = BoardComment.pluck(:id).sample(5)
