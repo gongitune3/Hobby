f26976dc4fbd4396f6450661739f8809abb74fc4
gongitune3 <rtdmwrtdmw@gmail.com>
2020-10-18 17:18:23 +0900

    add

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -5,11 +5,12 @@ class Admins::UsersController < ApplicationController
       end
 
       def show
-            @user = User.find(params[:id])
             # ユーザーの直近コメント表示
+            @user = User.find(params[:id])
+
+            # ユーザーおすすめ機能→自身がコメントした板に紐づくタグと同様のタグをもつ板にコメントしているユーザーを取得する
             order_comment = @user.board_comments.order(created_at: :desc).limit(3)
             @order_boards = Board.where(id: order_comment.pluck(:board_id)).distinct
-            # ユーザーおすすめ機能→自身がコメントした板に紐づくタグと同様のタグをもつ板にコメントしているユーザーを取得する
       end
 
 end
