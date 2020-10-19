69d847d8407e55a13a33d3884530b3faa2272db7
gongitune3 <rtdmwrtdmw@gmail.com>
2020-10-19 14:40:18 +0900

    add

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,7 @@
 class Tag < ApplicationRecord
+
     has_many :board_tags, dependent: :destroy
     has_many :boards, through: :board_tags
     validates :name, presence: true, length: { maximum: 14 }
+
 end
